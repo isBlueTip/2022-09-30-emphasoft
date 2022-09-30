41c21306021ee6872306e7ead9ac6761bae09d85
Simon Egorov <rhinoRofl@gmail.com>
2022-09-30 18:08:48 +0600

    Setup users admin panel

--- emphasoft_crud/users/admin.py
+++ emphasoft_crud/users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk',
+                    'username',
+                    'email',
+                    'first_name',
+                    'last_name',
+                    'is_staff',
+                    'is_active',
+                    )
+    list_editable = ('username',
+                     'email',
+                     'first_name',
+                     'last_name',
+                     'is_staff',
+                     'is_active',
+                     )
+    search_fields = ('username',
+                     'email',
+                     'first_name',
+                     'last_name',
+                     )
+    list_filter = ('is_staff',
+                   'is_active',
+                   )
+    empty_value_display = 'empty'
+
+
+admin.site.register(User, UserAdmin)
