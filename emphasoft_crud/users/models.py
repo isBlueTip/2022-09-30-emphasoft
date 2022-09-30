ce396c4bf2f152f1c3606f5ccdb070b3ad37d375
Simon Egorov <rhinoRofl@gmail.com>
2022-09-30 12:16:34 +0700

    Initial user model description

--- emphasoft_crud/users/models.py
+++ emphasoft_crud/users/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+ADMIN = 'admin'
+USER = 'user'
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        'Username',
+        max_length=150,
+        # min_length=1,  # TODO implement via min_length validator
+        # TODO pattern: ^[\w.@+-]+$
+    )
+    first_name = models.CharField(
+        'First name',
+        max_length=30,
+    )
+    last_name = models.CharField(
+        'last name',
+        max_length=150,
+    )
+    password = models.CharField(
+        'Password',
+        # TODO pattern: ^(?=.*[A-Z])(?=.*\d).{8,}$
+        max_length=150,
+        # min_length=1,  # TODO implement via min_length validator
+        blank=False,
+    )
+    is_active = models.BooleanField(  # TODO how to inherit attributes from parent class?
+        'Active',
+        default=True,
+        blank=False,
+    )
+    is_superuser = models.BooleanField(
+        'Superuser status',
+        default=False,
+        blank=False,
+    )
+
+    class Meta:
+        ordering = ['-id']
+        # verbose_name = 'пользователь'
+        # verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        if self.get_full_name():
+            return self.get_full_name()
+        return self.username
