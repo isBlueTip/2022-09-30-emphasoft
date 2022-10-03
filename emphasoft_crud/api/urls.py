1f1a44832a3e73093ae0c7401d31c5ad8d41131d
Simon Egorov <rhinoRofl@gmail.com>
2022-10-03 21:30:00 +0600

    Update token obtain urls

--- emphasoft_crud/api/urls.py
+++ emphasoft_crud/api/urls.py
@@ -1,6 +1,7 @@
 from api.users_views import UserViewSet
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework.authtoken import views
 
 
 router = SimpleRouter()
@@ -10,4 +11,5 @@ router.register('users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
