3d5c07e7a251c6a2bef1dd0379f2c7090c0d582f
Simon Egorov <rhinoRofl@gmail.com>
2022-10-04 11:06:26 +0600

    Create custom permission

---
+++ emphasoft_crud/api/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+
+
+class CreateOrSelfOrAdminOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to create new user or allow user or admin only
+    to update user info.
+    """
+
+    def has_permission(self, request, view):
+        if (request.method == 'POST' or
+                request.method in permissions.SAFE_METHODS or
+                (request.user and request.user.is_staff)):
+            return True
+
+    def has_object_permission(self, request, view, obj):
+        return (
+                request.method in permissions.SAFE_METHODS or
+                obj == request.user or
+                (request.user and request.user.is_staff))
